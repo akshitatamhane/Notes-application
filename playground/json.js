42160c41a888ee41a3bc13d1f6587dc4f8607c53
Akshita Tamhane <akshitatamhane93@gmail.com>
2018-06-14 19:54:25 +0530

    first commit

---
+++ playground/json.js
@@ -0,0 +1,28 @@
+//var obj={
+    //name:'akshita'
+//};
+
+//var stringObj =JSON.stringify(obj);
+//console.log(typeof stringObj);
+//console.log(stringObj);
+//var personString='{"name":"akshita","age":21}';
+//var person=JSON.parse(personString);
+//console.log(typeof person);
+//console.log(person);
+
+const fs =require ('fs');
+var originalNote ={
+
+    title:'some title',
+    body:'some body'
+};
+//originalNoteString
+var originalNoteString=JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString=fs.readFileSync('notes.json');
+var note=JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
+
+
